ec1905baff5ecc2d345e490ad73d3de89c83ee6f
ananjaemin <t01089416422@gmail.com>
2021-12-26 04:21:49 +0900

    ....

--- js/app.js
+++ js/app.js
@@ -1,11 +1,69 @@
-$("div.start_cover").delay(1000).fadeOut(700); //시작했을때 사라지는 logo
+ //시작했을때 사라지는 logo
 
 
-let colorIndicator = document.getElementById("color_indicator");
-const colorPicker = new iro.colorPicker("#color_picker",{
-    width:180, color:"#fff"
+let rgb_box_1 = document.getElementById("rgb_box_1");
+let colorIndicator = document.getElementById("color-indicator");
+
+
+
+const colorPicker = new iro.ColorPicker("#color-picker",{
+    layout: [
+        { 
+          component: iro.ui.Wheel,
+          options: {
+            wheelLightness : false,
+            width : 300,
+            borderColor: rgb_box_1,
+          }
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+                sliderType: 'value',
+            }
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+            //can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+                sliderType: 'red',
+            }
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+            //can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+                sliderType: 'green',
+            }
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+            //can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+                sliderType: 'blue',
+            }
+        },
+        
+      ]
+      
 });
-colorPicker.on("color:change", function(color){
-    colorIndicator.style.background = color.hexString;
+
+
+colorPicker.on(["color:init", "color:change"], function(color){
+    colorIndicator.style.backgroundColor = color.hexString;
+    rgb_box_1.value = color.hexString;
+    
+
+
 });
+
+
+function enterkey() {
+	if (window.event.keyCode == 13) {
+        colorIndicator.style.backgroundColor = rgb_box_1.value
+    }
+}
+
+
+
 
