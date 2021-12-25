43c14bfc34ba2ab59c88922c6142a3b86e7bbff5
ananjaemin <t01089416422@gmail.com>
2021-12-26 06:33:59 +0900

    commit all

--- color-of-the-year/src/App.js
+++ color-of-the-year/src/App.js
@@ -5,7 +5,7 @@ import Box from "./Box.js";
 function App() {
   return (
     <div>
-      <Box year={2020} color="black" />
+      <Box year={2020} color="blue" />
       <Box year={2021} color="blue" />
     </div>
   );
