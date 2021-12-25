2957efc7ca423106ef17423a708c6ca5d1bdec4e
minpeter <kali2005611@gmail.com>
2021-12-26 05:20:13 +0900

    med

--- color-of-the-year/src/App.js
+++ color-of-the-year/src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
 
-function App() {
-  return (
-    <div class="color_box">
-        <div class="color_box_in_text">
-          
-        </div>
-    </div>
+// function App() {
+//   return <h1></h1>;
+// }
 
+// export default App;
 
-  );
+import React from "react";
+import "./App.css";
+import "./Box.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="boxs">
+        <Box id={1233} year={2021} />
+      </div>
+    );
+  }
 }
 
 export default App;
