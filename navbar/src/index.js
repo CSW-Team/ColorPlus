0b15efd97e68a9c5b2ff779f1feaa0522de9c65e
minpeter <kali2005611@gmail.com>
2021-12-26 19:52:19 +0900

    Coty와 Navbar 연동

--- navbar/src/index.js
+++ navbar/src/index.js
@@ -1,18 +1,18 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import SignPage from "./pages/SignPage";
-import MainPage from "./pages/MainPage";
-import ProfilePage from "./pages/ProfilePage";
+import Cc from "./pages/Cc";
+import Ce from "./pages/Ce";
+import Coty from "./pages/Coty";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="cc" element={<SignPage />} />
-      <Route path="ce" element={<MainPage />} />
-      <Route path="coty" element={<ProfilePage />} />
+      <Route path="cc" element={<Cc />} />
+      <Route path="ce" element={<Ce />} />
+      <Route path="coty" element={<Coty />} />
     </Routes>
   </BrowserRouter>,
   rootElement
