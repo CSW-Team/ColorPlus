93a6dbce2dbc560e1e732cb969c0bfd114fd445c
minpeter <kali2005611@gmail.com>
2021-12-26 20:03:48 +0900

    colorPlus-build complite

--- color-of-the-year/src/App.js
+++ color-of-the-year/src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js";
-import Ce from "./pages/Ce.js";
 import Coty from "./pages/Coty.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" component={Home} />
-            <Route path="/ce" exact component={Ce} />
-            <Route path="/coty" component={Coty} />
-          </Routes>
-        </Router>
+        <Coty />
       </div>
     );
   }
