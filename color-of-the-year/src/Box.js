7e5ae925da208eb43bcbc4ca7786914ff8f9a004
ananjaemin <t01089416422@gmail.com>
2021-12-26 09:26:01 +0900

    완성

--- color-of-the-year/src/Box.js
+++ color-of-the-year/src/Box.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Box.css";
 
-function Box({ year, color }) {
+function Box({ year, color, color_name }) {
   return (
-    <div class="color_box" style={{ backgroundColor: color }}>
-      <div class="color_box_in_text">{year}</div>
+    <div>
+      <div class="color_box" style={{ backgroundColor: color }}>
+        <div class="color_box_in_text">{year}</div>
+      </div>
+      <div class="color_code_text">{color_name}</div>
     </div>
   );
 }
